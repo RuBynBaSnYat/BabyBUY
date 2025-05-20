b9ae2e9898c776def9102be9eba6645996b6d24d
Rubyn Basnyat <141rubynbasnyat@gmail.com>
2025-05-20 10:42:33 +0545

    This is babybuy app

---
+++ BabyBuy/app/src/main/java/com/ismt/babybuy/utils/ToastUtils.kt
@@ -0,0 +1,15 @@
+package com.ismt.babybuy.utils
+import android.content.Context
+import android.widget.Toast
+
+class ToastUtils {
+    companion object {
+        fun showToast(context: Context, text: String) {
+            Toast.makeText(
+                context,
+                text,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
